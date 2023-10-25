2642852a12af7d747dca2e36d16657108a8d4fc6
Artem Yakischik <a.yakischik@rarible.com>
2023-10-25 17:41:28 +0400

    feat: x-controller support

--- src/main/java/com/rarible/openapi/server/RaribleServerGenerator.java
+++ src/main/java/com/rarible/openapi/server/RaribleServerGenerator.java
@@ -1,10 +1,17 @@
 package com.rarible.openapi.server;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.PathItem;
 import org.openapitools.codegen.languages.KotlinSpringServerCodegen;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class RaribleServerGenerator extends KotlinSpringServerCodegen {
+
+    private static final String X_CONTROLLER = "x-controller";
 
     /**
      * Configures a friendly name for the generator.  This will be used by the generator
@@ -15,6 +22,39 @@ public class RaribleServerGenerator extends KotlinSpringServerCodegen {
     @Override
     public String getName() {
         return "rarible-server";
+    }
+
+    private final Map<String, String> tagMapping = new HashMap<>();
+
+    @Override
+    public void preprocessOpenAPI(OpenAPI openAPI) {
+        for (PathItem path : openAPI.getPaths().values()) {
+            for (Operation operation : path.readOperations()) {
+                Map<String, Object> extensions = operation.getExtensions();
+                if (extensions == null || !extensions.containsKey(X_CONTROLLER)) {
+                    continue;
+                }
+                String controller = extensions.get(X_CONTROLLER).toString();
+                for (String tag : operation.getTags()) {
+                    String exists = tagMapping.get(tag);
+                    if (exists != null && !controller.equals(exists)) {
+                        throw new IllegalArgumentException("'x-controller' value for operation "
+                                + operation.getOperationId() + " with tag " + tag + " set as '" + operation + "' "
+                                + "but this tag already mapped to controller with name '" + exists + "'");
+                    }
+                    tagMapping.put(tag, controller);
+                }
+            }
+        }
+    }
+
+    @Override
+    public String sanitizeTag(String tag) {
+        String controller = tagMapping.get(tag);
+        if (controller != null) {
+            return controller;
+        }
+        return super.sanitizeTag(tag);
     }
 
     public RaribleServerGenerator() {
